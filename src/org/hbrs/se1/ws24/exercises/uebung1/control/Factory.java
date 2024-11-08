28aceb66df75a09e8e5f54c22ffa2ced30478c4f
lewis <lewis.tekuh@smail.inf.h-brs.de>
2024-11-08 20:51:48 +0100

    erste erstellung

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/Factory.java
@@ -1,4 +1,8 @@
 package org.hbrs.se1.ws24.exercises.uebung1.control;
 
 public class Factory {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
